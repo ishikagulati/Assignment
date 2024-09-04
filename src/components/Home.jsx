380b7cbe8d5f19a178ca5e79252d03c70b6d5e26
Rishikesh <krishikesh369@gmail.com>
2024-09-05 02:16:18 +0530

    separator added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,15 @@ export default function Home() {
             <div className=" font-semibold w-1/2 bg-[#616161D1] border border-[#96BEE7] rounded-[27px]"></div>
         <div className="w-1/2 flex flex-col gap-10">
         <AboutCard/>
+        <div className="px-14">
+            <div className="bg-custom-gradient rounded-sm w-full px-6 h-[4px] shadow-custom"></div>
+        </div>
+
         <Gallery/>
+        <div className="px-14">
+            <div className="bg-custom-gradient rounded-sm w-full px-6 h-[4px] shadow-custom"></div>
+        </div>
+
         </div>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
         'custom-gradient': 'linear-gradient(180deg, rgba(40, 40, 40, 0.1) 0%, rgba(248, 248, 248, 0.1) 100%), linear-gradient(0deg, rgba(255, 255, 255, 0.05), rgba(255, 255, 255, 0.05))',
       },
       boxShadow: {
-        'custom': '0px 4px 4px 0px rgba(0, 0, 0, 0.33)', // rgba(0, 0, 0, 0.33) is equivalent to #00000054
+        'custom': '0px 4px 4px 0px rgba(0, 0, 0, 0.33)', 
       },
     },
   },
