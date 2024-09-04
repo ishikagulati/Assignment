e4c8079539fc833778724755ed7e6e9fe61cc4a4
Rishikesh <krishikesh369@gmail.com>
2024-09-04 18:55:05 +0530

    about section completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AboutCard from './components/About'
+import Home from './components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      Lunacal
+    <div className='p-4 w-full' style={{background: "linear-gradient(180deg, #373E44 -100%, #191B1F 100%", height:"895px", borderRadius:"28px"}}>
+      {/* <AboutCard/> */}
+      <Home/>
+      </div>
     </>
   )
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import AboutCard from "./About";
+
+
+export default function Home() {
+
+    return (
+        <div className="w-full flex flex-row justify-between">
+            <div className=" font-semibold w-1/2">h</div>
+        <div className="w-1/2">
+        <AboutCard/>
+        </div>
+        </div>
+    );
+};
+
+
